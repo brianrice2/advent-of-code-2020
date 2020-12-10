f5545a70dc09131bee128f96c016964d8068124a
Brian Rice <brianrice013@gmail.com>
2020-12-10 17:47:45 -0600

    Add day 6

---
+++ day6.py
@@ -0,0 +1,32 @@
+import re
+from collections import Counter
+
+# Load input data
+with open('day6/input.txt', 'r') as f:
+    responses = f.read()
+responses = responses.split('\n\n')
+
+
+# Part 1
+num_questions = 0
+for group in responses:
+    # Remove extra (non-letter) characters
+    group = re.sub(r'[^a-z]', '', group)
+
+    # Count number of unique letters
+    num_questions += len(''.join(set(group)))
+print(num_questions)
+
+
+# Part 2
+num_questions = 0
+for group in responses:
+    # Count number of total people per group
+    num_people = len(group.split('\n'))
+
+    # Identify letters with frequency equal to number of people
+    letter_counts = Counter(group)
+    for key, value in letter_counts.items():
+        if value == num_people:
+            num_questions += 1
+print(num_questions)
