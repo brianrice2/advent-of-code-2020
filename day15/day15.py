16e179a196c46923610c622b455f94959c86f1b3
Brian Rice <brianrice013@gmail.com>
2020-12-15 00:37:07 -0600

    Add day 15

---
+++ day15/day15.py
@@ -0,0 +1,26 @@
+from collections import defaultdict, deque
+
+inlist = [16,1,0,18,12,14,19]
+
+# In general: track the last two times a number's been said
+# Starting round of saying each input number
+indict = {}
+for i in range(len(inlist)):
+    indict[inlist[i]] = i + 1, None  
+last2turns = defaultdict(lambda: (None, None), indict)
+
+# Perform next turn automatically (always equals 0)
+most_recent = 0
+last2turns[most_recent] = (len(inlist) + 1, indict[most_recent][0]) if 0 in last2turns else (4, None)
+
+# Perform all turns thereafter
+stop = 30000000  # Use stop = 2020 for Part 1
+for turn in range(len(inlist) + 2, stop + 1):
+    if not last2turns[most_recent][1]:  # last time was 1st time
+        age = most_recent = 0
+    else:
+        age = turn - last2turns[most_recent][1] - 1
+    if turn == stop:
+        print('Turn ', turn, ': ', age, sep='')
+    last2turns[age] = turn, last2turns[age][0]
+    most_recent = age
